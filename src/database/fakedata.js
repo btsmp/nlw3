5a03942ea7510a858daa5f0b07debf55590f9489
BrunoSampaio <brunoctsb@gmail.com>
2021-02-08 15:12:15 -0300

    added content

---
+++ src/database/fakedata.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-9.653746",
+        lng: "-35.7221577",
+        name: "Lar das meninas",
+        description:
+            "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1610675863703-5fdd6c856031?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+            "https://images.unsplash.com/photo-1612364461060-38075479ec26?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+        ],
+        instructions:
+            "Venha como se sentir a vontade e traga muito amor e paciência para dar. ",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1",
+    },
+    {
+        id: 2,
+        name: "Lar dos meninos",
+        lat: "-9.653746",
+        lng: "-35.7321577",
+        description:
+            "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1610675863703-5fdd6c856031?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+            "https://images.unsplash.com/photo-1612364461060-38075479ec26?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+        ],
+        instructions:
+            "Venha como se sentir a vontade e traga muito amor e paciência para dar. ",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0",
+    },
+];
--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const  path = require('path');
+const express = require("express");
+const path = require("path");
+const pages = require("./pages.js");
 
 const server = express();
 
 server
-    .use(express.static('public'))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'hbs')
-    .get('/', (request, response) => {
-        return response.render('index')
-})
+    .use(express.static("public"))
+    .set("views", path.join(__dirname, "views"))
+    .set("view engine", "hbs")
+    .get("/", pages.index)
+    .get("/create-orphanage", pages.createOrphanage)
+    .get("/orphanage", pages.orphanage)
+    .get("/orphanages", pages.orphanages);
 
-server.listen(5500)
+server.listen(5500);
